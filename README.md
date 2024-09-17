2d7fa3bda3fab9c4fb89d7cbc82e3fc515f8b52f
Joy Harrington <joysyk23@gmail.com>
2024-09-17 09:01:34 -0500

    After solution

--- README.md
+++ README.md
@@ -8,5 +8,123 @@ A few seconds later, a new listing appears for a freelancer named Carol, who is 
 
 New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
 
-1. I need to create a list of freelancers with the common input of name, occupation, and starting price.
-2.
+// === State ===
+// SCREAMING_SNAKE_CASE is used to denote constants: variables that are "hard-coded"
+const NAMES = [
+"John",
+"Lenny",
+"Andrew",
+"Peter",
+"Anna",
+"Albert",
+"Adam",
+"Robert",
+];
+const OCCUPATIONS = [
+"gardener",
+"programmer",
+"teacher",
+"driver",
+"designer",
+"painter",
+"writer",
+"actor",
+];
+const MAX_FREELANCERS = 25;
+
+const freelancers = [
+{ name: "Dr. Slice", price: 25, occupation: "gardener" },
+{ name: "Dr. Pressure", price: 51, occupation: "programmer" },
+{ name: "Prof. Possibility", price: 43, occupation: "teacher" },
+{ name: "Prof. Prism", price: 81, occupation: "teacher" },
+{ name: "Dr. Impulse", price: 43, occupation: "teacher" },
+{ name: "Prof. Spark", price: 76, occupation: "programmer" },
+{ name: "Dr. Wire", price: 47, occupation: "teacher" },
+{ name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+let averagePrice = 0;
+
+/\*_ Adds a random freelancer to state _/
+function addFreelancer() {
+// Pick random name, occupation and price
+const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+const occupation =
+OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+const price = 1 + Math.floor(Math.random() \* 100);
+
+freelancers.push({ name, occupation, price });
+}
+
+/\*_ Updates average price according to freelancers in state _/
+function updateAveragePrice(state) {
+const total = freelancers.reduce(
+(acc, freelancer) => acc + freelancer.price,
+0
+);
+averagePrice = total / freelancers.length;
+}
+
+// === Render ===
+/\*_ Renders freelancers onto the page _/
+function renderFreelancers() {
+const $freelancers = freelancers.map((freelancer) => {
+const $tr = document.createElement("tr");
+
+    $tr.innerHTML = `
+    <td>${freelancer.name}</td>
+    <td>${freelancer.occupation}</td>
+    <td>$${freelancer.price}</td>
+    `;
+
+    // NOTE: We can set `innerHTML` directly, but it can be a security risk!
+    // You can accomplish the same goal with the code below
+
+    /*
+    const $nameTd = document.createElement("td")
+    $nameTd.textContent = freelancer.name;
+
+    const $occupationTd = document.createElement("td")
+    $occupationTd.textContent = freelancer.occupation;
+
+    const $priceTd = document.createElement("td")
+    $priceTd.textContent = freelancer.price;
+
+    $tr.replaceChildren(...[$nameTd, $occupationTd, $priceTd]);
+    */
+
+    return $tr;
+
+});
+
+const $tbody = document.querySelector(".freelancers tbody");
+  $tbody.replaceChildren(...$freelancers);
+}
+
+/\*_ Renders the average price _/
+function renderAveragePrice() {
+const $price = document.querySelector(".average_price");
+  $price.textContent = `$${averagePrice.toFixed(2)}`;
+}
+
+/\*_ Renders everything in state _/
+function render() {
+renderFreelancers();
+renderAveragePrice();
+}
+
+// === Script ===
+
+// Initial render
+updateAveragePrice();
+render();
+
+// Add a freelancer every second until max is reached
+const freelancerInterval = setInterval(function () {
+addFreelancer();
+updateAveragePrice();
+render();
+
+if (freelancers.length >= MAX_FREELANCERS) {
+clearInterval(freelancerInterval);
+}
+}, 1000);
--- index.js
+++ index.js
@@ -1,43 +1,100 @@
+const NAMES = [
+  "John",
+  "Lenny",
+  "Andrew",
+  "Peter",
+  "Anna",
+  "Albert",
+  "Adam",
+  "Robert",
+];
+const OCCUPATIONS = [
+  "Psychologist",
+  "programmer",
+  "teacher",
+  "driver",
+  "Therapist",
+  "painter",
+  "writer",
+  "actor",
+];
+const maxFreelancers = 25;
+
 const freelancers = [
   { name: "Alice", occupation: "Writer", price: 30 },
   { name: "Bob", occupation: "Teacher", price: 50 },
-  { name: "Patricia", occupation: "Teacher", price: 65 },
+  { name: "Patricia", occupation: "Gardner", price: 65 },
   { name: "Derek", occupation: "Programmer", price: 80 },
   { name: "Beatrice", occupation: "Therapist", price: 120 },
   { name: "Libby", occupation: "Psychologist", price: 110 },
   { name: "Brandon", occupation: "Handyman", price: 45 },
 ];
-// Setting up the containers
-const freelancerList = document.getElementById("freelancerList");
-const averagePrice = document.getElementById("averagePrice");
+let averagePrice = 0;
 
-// Calculate the average price
-function getAveragePrice() {
-  const totalPrice = freelancers.reduce(
-    (sum, freelancer) => sum + freelancer.price,
+/** Adds a random freelancer to state */
+function addFreelancer() {
+  // Pick random name, occupation and price
+  const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+  const occupation =
+    OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  const price = 1 + Math.floor(Math.random() * 100);
+
+  freelancers.push({ name, occupation, price });
+}
+
+/** Updates average price according to freelancers in state */
+function updateAveragePrice(state) {
+  const total = freelancers.reduce(
+    (acc, freelancer) => acc + freelancer.price,
     0
   );
-  return totalPrice / freelancers.length;
+  averagePrice = total / freelancers.length;
 }
-// Making it render
-function render() {
+
+// === Render ===
+/** Renders freelancers onto the page */
+function renderFreelancers() {
   const $freelancers = freelancers.map((freelancer) => {
-    const NameElement = document.createElement("li");
-    const nameText = document.createTextNode(freelancer.name);
-    NameElement.appendChild(nameText);
+    const $tr = document.createElement("tr");
 
-    const OccupationElement = document.createElement("li");
-    const occText = document.createTextNode(freelancer.occupation);
-    OccupationElement.appendChild(occText);
+    $tr.innerHTML = `
+    <td>${freelancer.name}</td>
+    <td>${freelancer.occupation}</td>
+    <td>$${freelancer.price}</td>
+    `;
 
-    const PriceElement = document.createElement("li");
-    const priceText = document.createTextNode(freelancer.price);
-    PriceElement.appendChild(priceText);
-
-    return NameElement;
+    return $tr;
   });
 
-  freelancerList.replaceChildren(...$freelancers);
+  const $tbody = document.querySelector(".freelancers tbody");
+  $tbody.replaceChildren(...$freelancers);
 }
+
+/** Renders the average price */
+function renderAveragePrice() {
+  const $price = document.querySelector(".average_price");
+  $price.textContent = `$${averagePrice.toFixed(2)}`;
+}
+
+/** Renders everything in state */
+function render() {
+  renderFreelancers();
+  renderAveragePrice();
+}
+
+// === Script ===
+
+// Initial render
+updateAveragePrice();
 render();
-// Doing the interval
+
+// Add a freelancer every second until max is reached
+const freelancerInterval = setInterval(function () {
+  addFreelancer();
+  updateAveragePrice();
+  render();
+
+  if (freelancers.length >= MAX_FREELANCERS) {
+    clearInterval(freelancerInterval);
+  }
+}, 1000);
